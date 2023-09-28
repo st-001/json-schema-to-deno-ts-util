1c651b5d08d2f7f9166b4fa3455b746e6ef96641
leigna <120130378+st-00000@users.noreply.github.com>
2023-09-28 01:32:39 +0100

    pizza

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { generatePizzaOrder, generatePizzaOrderSummary } from "./pizza.ts";
+
+const query = Deno.args[0];
+
+const pizzaOrder = await generatePizzaOrder(query);
+
+const pizzaOrderSummary = await generatePizzaOrderSummary(pizzaOrder);
+
+console.log(pizzaOrderSummary);
